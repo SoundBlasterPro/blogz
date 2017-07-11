f3fbe086dce3291f7cc567aa628c4dabee187cde
William Reed <william_reed@live.com>
2017-07-10 19:48:52 -0700

    changed server string

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:fred123@localhost:8889/blogz'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:fred123@localhost:8889/blogz'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'mysql+pymysql://blogz:test@localhost:8889/blogz')
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = "TheGoatSingsAtMidnight"
